04efd6765aa7e98912f60a708e7933a348880d55
JDev786 <90481759+JDev786@users.noreply.github.com>
2023-01-22 21:16:54 +0000

    first commit

--- app.js
+++ app.js
@@ -15,22 +15,28 @@
 //   res.end()
 // })
 
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req,res)=>{
+// const server = http.createServer((req,res)=>{
 
-  //res.write(`hey it's our homepage`)
-if(req.url === '/'){
-  res.end('Welcome to our home page')
-}
-if(req.url === '/about'){
-  res.end('our short history')
-}
- res.end(`
- <h1>oops</h1>
- <p>we can't find page we are looking for</p>
- <a href = "/"> back home</a>
- `) 
-})
+//   //res.write(`hey it's our homepage`)
+// if(req.url === '/'){
+//   res.end('Welcome to our home page')
+// }
+// if(req.url === '/about'){
+//   res.end('our short history')
+// }
+//  res.end(`
+//  <h1>oops</h1>
+//  <p>we can't find page we are looking for</p>
+//  <a href = "/"> back home</a>
+//  `) 
+// })
   
-server.listen(5000)
+// server.listen(5000)
+
+const _=require('lodash');
+
+const items = [1,[2,[3,[4]]]]
+const newItems =_.flattenDeep(items)
+console.log(newItems);
